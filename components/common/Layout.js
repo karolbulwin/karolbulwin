e96bc44d5b5bd1cfd94cc685b56ab6c6ff37abcd
Karol Bulwin <karolbulwin@gmail.com>
2019-07-01 12:01:12 +0200

    removed loading screen

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,32 +1,14 @@
-import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Header from "./Header";
 import Footer from "./Footer";
-import Spinner from "./Spinner";
 import "./Layout.scss";
 
 const Layout = ({ children }) => {
-	const [dataIsLoaded, setDataIsLoaded] = useState(false);
-	useEffect(() => {
-		const timer = setTimeout(() => {
-			setDataIsLoaded(true);
-		}, 600);
-		return () => {
-			clearTimeout(timer);
-		};
-	}, []);
-
 	return (
 		<>
-			{dataIsLoaded ? (
-				<>
-					<Header />
-					<main>{children}</main>
-					<Footer />
-				</>
-			) : (
-				<Spinner />
-			)}
+			<Header />
+			<main>{children}</main>
+			<Footer />
 		</>
 	);
 };
