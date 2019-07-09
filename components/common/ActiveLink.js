2c6151cf76c1213a5dec258cc6687a15cb894896
Karol Bulwin <karolbulwin@gmail.com>
2019-07-09 22:01:58 +0200

    changed colors -- removed yellow

--- components/common/ActiveLink.js
+++ components/common/ActiveLink.js
@@ -4,38 +4,38 @@ import PropTypes from "prop-types";
 import css from "./ActiveLink.module.scss";
 
 const ActiveLink = ({ children, router, href }) => {
-	const checkPath = () => {
-		const regex = /[^/]+/;
-		let result = router.pathname.match(regex);
-		result = result === null ? "/" : `/${result[0]}`;
-		return result;
-	};
-	const style = {
-		color: checkPath() === href ? "#c9d8d6" : "none"
-	};
+  const checkPath = () => {
+    const regex = /[^/]+/;
+    let result = router.pathname.match(regex);
+    result = result === null ? "/" : `/${result[0]}`;
+    return result;
+  };
+  const style = {
+    color: checkPath() === href ? "#6e9590" : "none"
+  };
 
-	const handleClick = e => {
-		e.preventDefault();
-		router.push(href);
-		window.scrollTo(0, 0);
-	};
+  const handleClick = e => {
+    e.preventDefault();
+    router.push(href);
+    window.scrollTo(0, 0);
+  };
 
-	return (
-		<li className={css.root} onClick={handleClick}>
-			<a className={css.link} href={href} style={style}>
-				{children}
-				{router.pathname === href ? (
-					<span className={css.srOnly}>(current)</span>
-				) : null}
-			</a>
-		</li>
-	);
+  return (
+    <li className={css.root} onClick={handleClick}>
+      <a className={css.link} href={href} style={style}>
+        {children}
+        {router.pathname === href ? (
+          <span className={css.srOnly}>(current)</span>
+        ) : null}
+      </a>
+    </li>
+  );
 };
 
 ActiveLink.propTypes = {
-	children: PropTypes.string.isRequired,
-	router: PropTypes.object.isRequired,
-	href: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired,
+  router: PropTypes.object.isRequired,
+  href: PropTypes.string.isRequired
 };
 
 export default withRouter(memo(ActiveLink));
--- components/common/NavButton.js
+++ components/common/NavButton.js
@@ -3,40 +3,40 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import css from "./NavButton.module.scss";
 
 const NavButton = ({ onClick, navActive }) => {
-	return (
-		<button
-			onClick={onClick}
-			className={navActive ? css.open : css.root}
-			aria-label="Toggle navigation"
-			style={{
-				padding: ".3em .3em .6em .6em",
-				backgroundColor: "transparent",
-				fontSize: "1em"
-			}}
-		>
-			<span>
-				{navActive === true ? (
-					<FontAwesomeIcon icon="times" />
-				) : (
-					<FontAwesomeIcon
-						icon="bars"
-						style={{
-							width: 19.6,
-							height: 22.4,
-							fontSize: "1em",
-							display: "inline-block",
-							verticalAlign: "-0.125em",
-							color: "#f9ae08"
-						}}
-					/>
-				)}
-			</span>
-		</button>
-	);
+  return (
+    <button
+      onClick={onClick}
+      className={navActive ? css.open : css.root}
+      aria-label="Toggle navigation"
+      style={{
+        padding: ".3em .3em .6em .6em",
+        backgroundColor: "transparent",
+        fontSize: "1em"
+      }}
+    >
+      <span>
+        {navActive === true ? (
+          <FontAwesomeIcon icon="times" />
+        ) : (
+          <FontAwesomeIcon
+            icon="bars"
+            style={{
+              width: 19.6,
+              height: 22.4,
+              fontSize: "1em",
+              display: "inline-block",
+              verticalAlign: "-0.125em",
+              color: "#c9d8d6"
+            }}
+          />
+        )}
+      </span>
+    </button>
+  );
 };
 NavButton.propTypes = {
-	onClick: PropTypes.func.isRequired,
-	navActive: PropTypes.bool.isRequired
+  onClick: PropTypes.func.isRequired,
+  navActive: PropTypes.bool.isRequired
 };
 
 export default NavButton;
