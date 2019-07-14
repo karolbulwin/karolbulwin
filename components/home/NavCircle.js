272dae5db25b2364e7e5dff896839d6e1a0e103a
Karol Bulwin <karolbulwin@gmail.com>
2019-07-14 19:41:21 +0200

    prevent others buttons to move after hover one

--- components/home/NavCircle.js
+++ components/home/NavCircle.js
@@ -3,25 +3,27 @@ import Router from "next/router";
 import css from "./NavCircle.module.scss";
 
 const NavCircle = ({ text, href }) => {
-	const handlerClick = () => {
-		Router.push({
-			pathname: href
-		});
-	};
-	return (
-		<button
-			aria-label={`Go to ${text}`}
-			className={css.root}
-			onClick={handlerClick}
-		>
-			{text}
-		</button>
-	);
+  const handlerClick = () => {
+    Router.push({
+      pathname: href
+    });
+  };
+  return (
+    <div className={css.root}>
+      <button
+        aria-label={`Go to ${text}`}
+        className={css.button}
+        onClick={handlerClick}
+      >
+        {text}
+      </button>
+    </div>
+  );
 };
 
 NavCircle.propTypes = {
-	text: ProptTypes.string.isRequired,
-	href: ProptTypes.string.isRequired
+  text: ProptTypes.string.isRequired,
+  href: ProptTypes.string.isRequired
 };
 
 export default NavCircle;
